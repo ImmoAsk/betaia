3e90ede255d329010aadab6308cb9d8aa1ebfd9e
AJKossi <kjadanou@gmail.com>
2024-01-12 23:37:42 +0000

    modified NextAuth.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,8 +19,8 @@ const login = async (username, password) => {
         accessToken: res.data.data.login.access_token,
         expiredAt: res.data.data.login.expires_in,
       }
-      //console.log(u);
-       return u ;
+      console.log(u);
+      return u ;
     });
     
 };
@@ -60,6 +60,7 @@ export const authOptions = {
         if (url.startsWith("/")) return `${baseUrl}${url}`
         // Allows callback URLs on the same origin
         else if (new URL(url).origin === baseUrl) return url
+        console.log(baseUrl);
         return baseUrl
       }
     },
