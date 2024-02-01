a3df4594c2ce8b5a15748ad7d882e8072bcf5a3d
Kossi ADANOU <kjadanou@gmail.com>
2024-02-01 16:23:24 +0000

    updated projects component

--- pages/tg/account-projects.js
+++ pages/tg/account-projects.js
@@ -20,7 +20,7 @@ const AccountProjectsPage = ({ _userProperties }) => {
   const [propertyModal, setPropertyModal] = useState({});
 
   const { data: session } = useSession();
-  const userProperties = buildPropertiesArray(_userProperties);
+  const userProperties = _userProperties;
   const handleEditPropertyModal = () => {
     //e.preventDefault();
     if (session) {
@@ -61,8 +61,8 @@ const AccountProjectsPage = ({ _userProperties }) => {
             Supprimer tout
           </a>
         </div>
-        <p className='pt-1 mb-4'>Trouver ici tous les projets immobiliers des clients 
-        comme recherche de logememt, achat....</p>
+        <p className='pt-1 mb-4'>Trouver ici tous les projets immobiliers des clients
+          comme recherche de logememt, achat....</p>
 
         {/* Nav tabs */}
         <Nav
@@ -91,54 +91,52 @@ const AccountProjectsPage = ({ _userProperties }) => {
         </Nav>
 
         {/* List of properties or empty state */}
-        {userProperties.length ? userProperties.map((property, indx) => (
-          <PropertyCard
-            key={indx}
-            href={property.href}
-            images={property.images}
-            category={property.category}
-            title={property.title}
-            location={property.location}
-            price={property.price}
-            badges={property.badges}
-            footer={[
-              ['fi-bed', property.amenities[0]],
-              ['fi-bath', property.amenities[1]],
-              ['fi-car', property.amenities[2]]
-            ]}
-            dropdown={[
-              {
-                // href: '#', // Optionally pass href prop to convert dropdown item to Next link
-                icon: 'fi-edit',
-                label: 'Editer',
-                props: {
-                  onClick: (event) => {
-                    event.stopPropagation();
-                    event.preventDefault();
-                    setPropertyModal(property);
-                    handleEditPropertyModal();
-                  }
-                }
-              },
-              {
-                icon: 'fi-flame',
-                label: 'Promouvoir',
-                props: { onClick: () => console.log('Promote property') }
-              },
-              {
-                icon: 'fi-power',
-                label: 'Rendre invisible',
-                props: { onClick: () => console.log('Deactivate property') }
-              },
-              {
-                icon: 'fi-trash',
-                label: 'Rendre indisponible',
-                props: { onClick: () => console.log('Deactivate property') }
-              }
-            ]}
-            horizontal
-            className={indx === userProperties.length - 1 ? '' : 'mb-4'}
-          />
+        {userProperties.length ? userProperties.map((project, indx) => (
+          <div class="pb-2">
+            <div class="card bg-secondary card-hover">
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-start mb-2">
+                  <div class="d-flex align-items-center">
+                    <span class="fs-sm text-dark opacity-80 px-1">{project.project_name}</span>
+                    <span class="badge bg-faded-accent rounded-pill fs-sm ms-2">{project.project_category}</span>
+                  </div>
+                  <div class="dropdown content-overlay">
+                    <button type="button" class="btn btn-icon btn-light btn-xs rounded-circle shadow-sm" id="contextMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="fi-dots-vertical"></i>
+                    </button>
+                    <ul class="dropdown-menu my-1" aria-labelledby="contextMenu">
+                      <li>
+                        <button type="button" class="dropdown-item">
+                          <i class="fi-heart opacity-60 me-2"></i>
+                          Traiter 
+                        </button>
+                      </li>
+                      <li>
+                        <button type="button" class="dropdown-item">
+                          <i class="fi-x-circle opacity-60 me-2"></i>
+                          Archiver
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <h3 class="h6 card-title pt-1 mb-3">
+                  <a href="#" class="text-nav stretched-link text-decoration-none">{project.description}</a>
+                </h3>
+                <div class="fs-sm">
+                  <span class="text-nowrap me-3">
+                    <i class="fi-calendar text-muted me-1"> </i>
+                    {project.start_date}
+                  </span>
+                  <span class="text-nowrap me-3">
+                    <i class="fi-cash fs-base text-muted me-1"></i>
+                    {project.statut}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
         )) : <div className='text-center pt-2 pt-md-4 pt-lg-5 pb-2 pb-md-0'>
           <i className='fi-home display-6 text-muted mb-4'></i>
           <h2 className='h5 mb-4'>Vous n'avez aucun bien immobilier enrollé!</h2>
@@ -161,22 +159,22 @@ export async function getServerSideProps(context) {
   if (session.user) {
     const userid = session ? session.user.id : 0;
     // Fetch data from external API
-    var dataAPIresponse = await fetch(`https://immoaskbetaapi.omnisoft.africa/public/api/v2?query={getUserProperties(user_id:${userid},first:10,orderBy:{column:NUO,order:DESC}){data{surface,badge_propriete{badge{badge_name,badge_image}},id,nuo,usage,offre{denomination},categorie_propriete{denomination},pays{code},piece,titre,garage,cout_mensuel,ville{denomination},wc_douche_interne,cout_vente,quartier{denomination},visuels{uri}}}}`);
+    var dataAPIresponse = await fetch(`https://immoaskbetaapi.omnisoft.africa/public/api/v2?query={getProjectsByKeyWords(statut:2){project_category,project_name,project_document,description,statut,final_date,start_date}}`);
     var _userProperties = await dataAPIresponse.json();
 
-    _userProperties = _userProperties.data.getUserProperties.data;
-
+    _userProperties = _userProperties.data.getProjectsByKeyWords;
+    console.log(_userProperties);
     return {
-      props: { _userProperties},
+      props: { _userProperties },
     }
 
   } else {
     return {
       redirect: {
-          destination: '/auth/signin',
-          permanent: false,
+        destination: '/auth/signin',
+        permanent: false,
       },
-  };
+    };
   }
 }
 export default AccountProjectsPage
