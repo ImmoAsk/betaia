ab501d177b9063e77392ed0f5692634d15568507
Kossi ADANOU <kjadanou@gmail.com>
2024-02-18 17:20:09 +0000

    added user properties into modal

--- components/iacomponents/RProjectReplyModal.js
+++ components/iacomponents/RProjectReplyModal.js
@@ -6,7 +6,10 @@ import Button from 'react-bootstrap/Button'
 import CloseButton from 'react-bootstrap/CloseButton'
 import ImageLoader from '../ImageLoader'
 import PasswordToggle from '../PasswordToggle'
-
+import IAPaginaation from './IAPagination'
+import { useSession,getSession } from 'next-auth/react'
+import { useUserProperties } from '../../customHooks/realEstateHooks'
+import { buildPropertiesArray } from '../../utils/generalUtils'
 const RProjectReplyModal = ({ project, onSwap, pillButtons, ...props }) => {
 
     // Form validation
@@ -19,7 +22,12 @@ const RProjectReplyModal = ({ project, onSwap, pillButtons, ...props }) => {
         }
         setValidated(true);
     }
-
+    const { data: session } = useSession();
+    //const [userProperties, setUserProperties] = useState([]);
+    //console.log(session?.user?.id);
+    const { data: user_Properties }= useUserProperties(session?.user?.id)
+  
+    const userProperties = buildPropertiesArray(user_Properties?.data);
     return (
         <Modal {...props} className='signin-modal'>
             <Modal.Body className='px-0 py-2 py-sm-0'>
@@ -29,26 +37,25 @@ const RProjectReplyModal = ({ project, onSwap, pillButtons, ...props }) => {
                     className='position-absolute top-0 end-0 mt-3 me-3'
                 />
                 <div className='row mx-0 align-items-center'>
-                    <h2 className='h3 mb-4 mb-sm-5'>Participer a un projet immobilier</h2>
+                    <h2 className='h3 mb-4 mb-sm-5'>Participer à un projet immobilier</h2>
                     <div className='col-md-3 border-end-md p-4 p-sm-5'>
                         <div className='d-flex justify-content-center'>
-                            Contact du Maitre doeuvre
+                            Maitre d'ouvrage
                         </div>
                     </div>
                     <div className='col-md-6 border-end-md px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5'>
                      {project.description}
                     </div>
                     <div className='col-md-3 px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5'>
-                        Echeance
+                        {project.final_date}
                     </div>
                 </div>
                 <div className='row mx-0 align-items-center'>
-                    Selectionner aumoins un bien immobilier dans votre stock
-                    qui repond au prjet immobilierdu client
+                    Sélectionner au moins un bien immobilier dans votre stock qui répond au projet immobilier du client
                 </div>
-                <div className='row mx-0 align-items-center'>
-                    Listing des biens de l'agent avec la pagination
-                </div>
+                
+                <IAPaginaation dataPagineted={userProperties}/>
+                
                 <div className='row mx-0 align-items-center'>
                     <Button variant={`outline-info ${pillButtons ? 'rounded-pill' : ''} w-100 mb-3`}>
                         <i className='fi-google fs-lg me-1'></i>
@@ -59,5 +66,20 @@ const RProjectReplyModal = ({ project, onSwap, pillButtons, ...props }) => {
         </Modal>
     )
 }
-
+/* export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+          destination: '/auth/signin',
+          permanent: false,
+      }
+    }
+  }
+  else{
+    const { data: user_Properties }= useUserProperties(session?.user?.id)
+    console.log(user_Properties);
+    return { props: { session } };
+  }
+} */
 export default RProjectReplyModal
