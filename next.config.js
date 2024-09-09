b0efa6871eddc01f63e29b53fca2b032d09a6b60
Kossi ADANOU <kjadanou@gmail.com>
2024-09-09 17:53:51 +0000

    update prorealestateagency package

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+ module.exports = {
   reactStrictMode: true,
   output: 'standalone',
   //swcMinify: true,
@@ -15,4 +15,55 @@ module.exports = {
       },
     ];
   },
-}
+} 
+/* const cors = require('cors');
+
+const allowedOrigins = ['https://beta.immoask.com', 'https://immoaskbetaapi.omnisoft.africa'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+module.exports = {
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    domains: ['immoask.com', 'immoaskbetaapi.omnisoft.africa', 'omnisoft.africa'],
+    imageSizes: [48, 64, 88, 96, 128, 256, 384, 416],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/tg',
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/public/api/(.*)', // Apply these headers to all API routes
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Adjust according to your needs
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+}; */
