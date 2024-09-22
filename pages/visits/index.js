c8d14ab5dbc96961a0be32f541e7979f55c9abaa
felix atoma <felixatoma2@gmail.com>
2024-09-22 09:49:29 +0000

    set a calandar for a property visit

---
+++ pages/visits/index.js
@@ -0,0 +1,10 @@
+import PropertyVisitForm from '../../components/../'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Schedule a Property Visit</h1>
+      <PropertyVisitForm />
+    </div>
+  );
+}
