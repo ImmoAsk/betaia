a64ff53e95538998b31654e13291cc0dd3ceb077
Kossi ADANOU <kjadanou@gmail.com>
2024-10-18 12:10:44 +0000

    adding PayVisit feature

--- components/iacomponents/PayVisitModal.js
+++ components/iacomponents/PayVisitModal.js
@@ -1,24 +1,92 @@
 import { useState } from 'react'
-import Link from 'next/link'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import CloseButton from 'react-bootstrap/CloseButton'
-import ImageLoader from '../ImageLoader'
-import PasswordToggle from '../PasswordToggle'
+import CardProperty from './CardProperty'
+import FormControl from 'react-bootstrap/FormControl';
+import { createPropertyObject } from '../../utils/buildPropertiesArray'
+import { getSession, useSession } from 'next-auth/react'
+import PhoneInput from 'react-phone-input-2';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-phone-input-2/lib/style.css';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { set } from 'nprogress'
+const PayVisitModal = ({ property, onSwap, pillButtons, ...props }) => {
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [visitDate, setVisitDate] = useState('');
+  const [hourVisit, setHourVisit] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [validated, setValidated] = useState(false);
+  const [visiteNotification, setVisiteNotification] = useState(null);
 
-const PayVisitModal = ({ property,onSwap, pillButtons, ...props }) => {
+  // Check if the form is valid
+  const isFormValid = (email && phone && visitDate && firstName);
+  const { data: session } = useSession();
+  // Form submission handler
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  // Form validation
-  const [validated, setValidated] = useState(false)
-  const handleSubmit = (event) => {
-    const form = event.currentTarget
-    if (form.checkValidity() === false) {
-      event.preventDefault()
-      event.stopPropagation()
+    // Validate form inputs
+    if (!isFormValid) {
+      console.log("Le formulaire n'est pas valide. Veuillez vérifier les champs.");
+      setValidated(true);
+      return;
     }
+
+    // Collect form data
+    const formData = {
+      email,
+      phone,
+      visitDate,
+      hourVisit,
+      firstName
+    };
+
+    // Prepare GraphQL mutation for rent visite
+
+    const visite_data = {
+      query: `mutation PayVisite($input: VisiteInput!) {
+        createVisite(input: $input) {
+          id
+        }
+      }`,
+      variables: {
+        input: {
+          email_visitor: session ? "" : formData.email,
+          telephone_visitor: session ? "" : formData.phone,
+          user_id: session ? session.user.id : 0,
+          date_visite: formData.visitDate,
+          heure_visite: formData.hourVisit,
+          propriete_id: Number(property.id),
+          proprietaire_id: Number(property?.user?.id),
+          fullname_visitor: session ? "" : formData.firstName,
+        }
+      }
+    };
+    console.log(visite_data);
+    try {
+      const response = await axios.post('https://immoaskbetaapi.omnisoft.africa/public/api/v2', visite_data, {
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      if (Number(response.data?.data?.createVisite?.id) >= 1) {
+        setVisiteNotification("Votre visite a été envoyée avec succès. Vous serez contacté sous peu.");
+        // Redirect or perform any other actions needed
+        //router.push("/thank-you");
+      }
+    } catch (error) {
+      console.error("Error during visite:", error);
+    }
+
     setValidated(true);
-  }
+  };
+
+  const propertyCard = createPropertyObject(property);
+
 
   return (
     <Modal {...props} className='signin-modal'>
@@ -28,52 +96,117 @@ const PayVisitModal = ({ property,onSwap, pillButtons, ...props }) => {
           aria-label='Close modal'
           className='position-absolute top-0 end-0 mt-3 me-3'
         />
-        <div className='row mx-0 align-items-center'>
+        <div className='row mx-0'>
           <div className='col-md-6 border-end-md p-4 p-sm-5'>
-            <h2 className='h3 mb-4 mb-sm-5'>Visite de : N°{property.nuo} {property.categorie_propriete.denomination} à {property.offre.denomination} | {property.surface} m²</h2>
-            <div className='d-flex justify-content-center'>
-              <ImageLoader
-                src='/images/signin-modal/signin.svg'
-                width={344}
-                height={292}
-                alt='Illusration'
-              />
-            </div>
-            <div className='mt-4 mt-sm-5'>Avant de faire une visite <a href='#' onClick={onSwap}>Vérifier la disponibilité</a></div>
-          </div>
-          <div className='col-md-6 px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5'>
-            <Button variant={`outline-info ${pillButtons ? 'rounded-pill' : ''} w-100 mb-3`}>
-              <i className='fi-google fs-lg me-1'></i>
-              Planifier une visite
-            </Button>
-            <Button variant={`outline-info ${pillButtons ? 'rounded-pill' : ''} w-100 mb-3`}>
-              <i className='fi-facebook fs-lg me-1'></i>
-              Sign in with Facebook
-            </Button>
+            <h2 className='h3 mb-2 mb-sm-2'>Visite d'un bien immobilier</h2>
+
             <div className='d-flex align-items-center py-3 mb-3'>
-              <hr className='w-100' />
-              <div className='px-3'>Or</div>
-              <hr className='w-100' />
+              <CardProperty property={propertyCard} />
             </div>
+            <div className='mt-2 mt-sm-2'>
+              Avant de visiter, <a href='#' onClick={onSwap}>Vérifier la disponibilité</a>
+            </div>
+          </div>
+
+          <div className='col-md-6 p-4 p-sm-5'>
+            <h3 className='h4'>
+              Vous planifiez une visite pour le bien immobilier N° {property.nuo}.
+            </h3>
+
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              <Form.Group controlId='si-email' className='mb-4'>
-                <Form.Label>Email address</Form.Label>
+              <Form.Group controlId='si-offer' className='mb-2'>
+                <Form.Label>Quand voulez-vous visiter ?</Form.Label>
                 <Form.Control
-                  type='email'
-                  placeholder='Enter your email'
-                  required
+                  as={DatePicker}
+                  selected={visitDate}
+                  minDate={new Date()}
+                  onChange={(date) => setVisitDate(date)}
+                  placeholderText='Selectionner une date'
+                  className='form-control'
                 />
+                <Form.Control.Feedback type="invalid">
+                  Veuillez saisir une offre valide.
+                </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group className='mb-4'>
-                <div className='d-flex align-items-center justify-content-between mb-2'>
-                  <Form.Label htmlFor='si-password' className='mb-0'>Password</Form.Label>
-                  <Link href='#'>
-                    <a className='fs-sm'>Forgot password?</a>
-                  </Link>
-                </div>
-                <PasswordToggle id='si-password' placeholder='Enter password' required />
+
+              <Form.Group controlId='si-offer' className='mb-2'>
+                <Form.Label>A quelle heure voulez-vous visiter ?</Form.Label>
+                <FormControl
+                  as={DatePicker}
+                  selected={hourVisit}
+                  minDate={new Date()}
+                  onChange={(date) => setHourVisit(date)}
+                  placeholderText='Selectionner une date'
+                />
+                <Form.Control.Feedback type="invalid">
+                  Veuillez saisir une offre valide.
+                </Form.Control.Feedback>
               </Form.Group>
-              <Button type='submit' size='lg' variant={`primary ${pillButtons ? 'rounded-pill' : ''} w-100`}>Sign in</Button>
+
+
+
+              {!session && (
+                <>
+                  <Form.Group className='mb-2'>
+                    <Form.Label>Numéro de téléphone</Form.Label>
+                    <PhoneInput
+                      country={'tg'}
+                      value={phone}
+                      onChange={(phone) => setPhone(phone)}
+                      enableSearch={true}
+                      inputProps={{
+                        name: 'phone',
+                        required: true,
+                        autoFocus: true,
+                        className: 'form-control w-100 form-control-lg',
+                      }}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Veuillez saisir un numéro de téléphone valide.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+                  <Form.Group controlId='si-email' className='mb-2'>
+                    <Form.Label>Votre email ?</Form.Label>
+                    <Form.Control
+                      type='email'
+                      name='email'
+                      placeholder='Saisir votre email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Veuillez saisir une adresse email valide.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+                  <Form.Group controlId='si-firstname' className='mb-2'>
+                    <Form.Label>Votre prénom ?</Form.Label>
+                    <Form.Control
+                      type='text'
+                      name='firstname'
+                      placeholder='Saisir votre prénom'
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Veuillez saisir votre prénom.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </>
+              )}
+
+              <Button
+                type='submit'
+                disabled={!isFormValid}
+                size='lg'
+                variant={`primary ${pillButtons ? 'rounded-pill' : ''} w-100`}
+              >
+                Négocier le loyer
+              </Button>
+              {visiteNotification && <div className="alert alert-success mt-3">{visiteNotification}</div>}
             </Form>
           </div>
         </div>
@@ -82,4 +215,4 @@ const PayVisitModal = ({ property,onSwap, pillButtons, ...props }) => {
   )
 }
 
-export default PayVisitModal
+export default PayVisitModal;
