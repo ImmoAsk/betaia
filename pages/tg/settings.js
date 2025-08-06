55c8572c61afbd168d24a66533e6ad0fa80967e0
Kossi ADANOU <kjadanou@gmail.com>
2025-08-06 10:26:13 +0000

    modified real estate setting displaying

--- pages/tg/settings.js
+++ pages/tg/settings.js
@@ -1,33 +1,25 @@
-//create a Nextjs component for the settings page of a Telegram bot
-//this component should have 3 card section per row
-//Section 1: Mise a jour de l'agence immobilière
-//Section 2: Visibilite des fonctionnalités
-//Section 3: Disonibilité des visites immobilières
-//Section 1 is a form to update the agency information: name, address, phone number, email,logo
-//Section 2 is a toggle to enable or disable features
-//Section 3 is a toggle to enable or disable property visits
-// pages/settings.js
+
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 import RealEstateOrgForm from '../../components/iacomponents/Settings/RealEstateOrgFrom';
 import FeaturesToggle from '../../components/iacomponents/Settings/FeaturesToggle';
 import VisitsToggle from '../../components/iacomponents/Settings/VisitsToggle';
 import RealEstateAccountLayout from '../../components/partials/RealEstateAccountLayout';
 import RealEstatePageLayout from '../../components/partials/RealEstatePageLayout';
+import { useSession, getSession } from 'next-auth/react';
 export default function SettingsPage() {
+    const { data: session } = useSession();
+    const role = session?.user?.roleId;
   return (
     <RealEstatePageLayout pageTitle='Paramètres' activeNav='VisitProperties' userLoggedIn>
         <RealEstateAccountLayout accountPageTitle='Paramètres'>
             
             <Row>
+                {(role === '1200' || role === '1233' || role === '1234' || role === '1235' || role === '1232') && (
                 <Col md={4} className="mb-4">
-                
-                    
                     <RealEstateOrgForm />
-                    
-                
-                </Col>
+                </Col>)}
                 <Col md={4} className="mb-4">
      
                     <FeaturesToggle />
